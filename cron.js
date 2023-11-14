6c30e4e94831c250da47051a3e121a9b88008824
onlydrinkbrainmilk <146143765+onlydrinkbrainmilk@users.noreply.github.com>
2023-11-13 20:51:38 -0500

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron')
-cron.schedule('*/1 * * * * *', callback)
+cron.schedule('0 22 * * 1-5', callback)
 
 function callback(){
     console.log('running every 10 seconds')
