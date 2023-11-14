f725879ccbbd3be9ec33086711a1d38f113c54d2
onlydrinkbrainmilk <146143765+onlydrinkbrainmilk@users.noreply.github.com>
2023-11-13 23:41:41 -0500

    update

--- cron-notify.js
+++ cron-notify.js
@@ -2,6 +2,9 @@ require('dotenv').config({ path: './key.env' })
 var cron = require('node-cron');
 var Pushover = require('node-pushover')
 
+var randomNum;
+var fs = require('fs');
+var cat = JSON.parse(fs.readFileSync('./cat.json','utf8'));
 var push = new Pushover({
     token: process.env.APP_TOKEN,
     user: process.env.USER_KEY
@@ -9,8 +12,9 @@ var push = new Pushover({
 cron.schedule('*/10 * * * * *', sendNotification)
 
 function sendNotification() {
-    push.send('FEED ME! FEED ME!', 'please remember to open a can when you get home 🤌')}
-
+    randomNum = Math.floor(Math.random()*9);
+    push.send(cat[randomNum].subject, cat[randomNum].message)
+}
 function callback(){
     console.log('running every 10 seconds')
 }
